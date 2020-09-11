9122ac4f95c58de1902b007ebb127d080ed3ea2f
bekir <bekir.karahodzic@outlook.com>
2020-09-11 17:53:43 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 
+import {Switch,Route} from "react-router-dom";
+
+const HatsPage = (props) =>{
+  console.log(props)
+  return(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  )
+}
+
 function App() {
   return (
     <div >
-  <HomePage></HomePage>
+       {/* when sees match just renders that nothin else */}
+       
+      <Route exact path="/" component={HomePage}></Route> {/* if you put exact(boolean) path must be same to render home page*/ }
+      <Route  path="/hats" component={HatsPage}></Route>  
+      
+      
     </div>
   );
 }
