fc8dd6e2cceb26b448b6f5550f3e9d4700e16823
bekir <bekir.karahodzic@outlook.com>
2020-09-12 22:07:07 +0200

    updatin our app with shop page collection preview component and collection item cmponent

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react';
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.component";
 import {Switch,Route} from "react-router-dom";
 
-const HatsPage = (props) =>{
-  console.log(props)
-  return(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  )
-}
 
 function App() {
   return (
     <div >
-       {/* when sees match just renders that nothin else */}
        
       <Route exact path="/" component={HomePage}></Route> {/* if you put exact(boolean) path must be same to render home page*/ }
-      <Route  path="/hats" component={HatsPage}></Route>  
+      <Route  path="/shop" component={ShopPage}></Route>  
       
       
     </div>
