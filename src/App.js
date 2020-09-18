3e3d3fdebd0d6c944c4a36d47043f205cac3b933
bekir <bekir.karahodzic@outlook.com>
2020-09-18 19:50:19 +0200

    added redux and implemented user reducer and user action to header and app component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 import React from 'react';
 import './App.css';
-
+import {connect} from "react-redux";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import {Switch,Route} from "react-router-dom";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth,createUserProfileDocument } from "./firebase/firebase.utils"; 
-
+import {setCurrentUser} from "./redux/user/user.actions";
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-
-    this.state={
-      currentUser:null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount(){ //last user that is logged in with google
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser : {
+           setCurrentUser({
               id:snapShot.id, 
               ...snapShot.data()
-            }
-          })
+            })
+          
                 })
       }else{
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     }  
     });
   }
@@ -48,7 +41,7 @@ class App extends React.Component {
 
     return (
       <div >
-        <Header currentUser={this.state.currentUser}></Header>
+        <Header ></Header>
          <Switch>
         <Route exact path="/" component={HomePage}></Route> {/* if you put exact(boolean) path must be same to render home page*/ }
         <Route  path="/shop" component={ShopPage}></Route>  
@@ -59,6 +52,10 @@ class App extends React.Component {
 
   }
  
-}
+} 
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser : user => dispatch(setCurrentUser(user))//whatewer you passing in dispatch it is gonna be action object that is assed to evry reducer
+})
+
+export default connect(null,mapDispatchToProps) (App);
