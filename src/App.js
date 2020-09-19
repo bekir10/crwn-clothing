9b0709a3b69e6d32cdd280b33f79f6092055b976
bekir <bekir.karahodzic@outlook.com>
2020-09-19 10:21:17 +0200

    updationg our app to redirect to home if user is signed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {connect} from "react-redux";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import {Switch,Route} from "react-router-dom";
+import {Switch,Route, Redirect} from "react-router-dom";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth,createUserProfileDocument } from "./firebase/firebase.utils"; 
@@ -45,7 +45,9 @@ class App extends React.Component {
          <Switch>
         <Route exact path="/" component={HomePage}></Route> {/* if you put exact(boolean) path must be same to render home page*/ }
         <Route  path="/shop" component={ShopPage}></Route>  
-        <Route  path='/signin' component={SignInAndSignUp}></Route>  
+        <Route exact path='/signin' render={() => 
+          this.props.currentUser ?
+          (<Redirect to ="/"></Redirect>) : (<SignInAndSignUp></SignInAndSignUp>) } ></Route>  
         </Switch>
       </div>
     );
@@ -54,8 +56,12 @@ class App extends React.Component {
  
 } 
 
+const mapStateToProps = ({user}) =>({
+ currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch =>({
   setCurrentUser : user => dispatch(setCurrentUser(user))//whatewer you passing in dispatch it is gonna be action object that is assed to evry reducer
 })
 
-export default connect(null,mapDispatchToProps) (App);
+export default connect(mapStateToProps,mapDispatchToProps) (App);
