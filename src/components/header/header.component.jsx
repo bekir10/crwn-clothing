e73535c26ee4e012035782f53b95f41fe76feef7
bekir10 <bekir.karahodzic@outlook.com>
2020-10-12 11:18:30 +0200

    adding styled components css in js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,46 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {ReactComponent as Logo} from "../assets/crown.svg";
 import {auth} from "../../firebase/firebase.utils";
 import {connect} from "react-redux";
-import "./header.styles.scss";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import {createStructuredSelector} from "reselect";
 import {selectCartHidden} from "../../redux/cart/cart.selectors"
 import {selectCurrentUser} from "../../redux/user/user.selectors"
-
+import {HeaderContainer,LogoContainer,OptionsContainer,OptionDiv,OptionLink} from "./header.styles"
 const Header = ({currentUser, hidden}) =>(
-    <div className="header">
+    <HeaderContainer>
 
-<Link to="/" className="logo-cotainer">
-    <Logo className="logo">
+<LogoContainer to="/">
+    <Logo className="logo"> </Logo>
+</LogoContainer>
 
-    </Logo>
-</Link>
-
-<div className="options">
-    <Link to="/shop" className="option">
-        SHOP
-    </Link>
-    <Link to="/shop" className="option">
-        CONTACT
-    </Link>
+<OptionsContainer>
+    <OptionLink to="/shop">SHOP</OptionLink>
+    <OptionLink to="/shop">CONTACT</OptionLink>
     {
         currentUser ? //if currentuser is object it will evaluate div else link
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+        <OptionLink as="div" onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
         :
-        <Link className="option" to="/signin">SIGN IN</Link>
+        <OptionLink  to="/signin">SIGN IN</OptionLink>
     }
     <CartIcon></CartIcon>
-</div>
+</OptionsContainer>
+
 {
     hidden?null:
     <CartDropdown></CartDropdown>
 
 }
 
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector ({
