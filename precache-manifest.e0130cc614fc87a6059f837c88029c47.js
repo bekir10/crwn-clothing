7dd00b8df804391ad64179d089b5486c43500727
bekir10 <bekir.karahodzic@outlook.com>
2020-11-05 17:41:18 +0100

    Updates

---
+++ precache-manifest.e0130cc614fc87a6059f837c88029c47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a6266bfb4921eea16c74c1832fed0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bad1c61d157fd0b36006",
+    "url": "/static/css/main.4cc3f55d.chunk.css"
+  },
+  {
+    "revision": "97bbff4f0db056a81e75",
+    "url": "/static/js/2.f9c22e3b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.f9c22e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad1c61d157fd0b36006",
+    "url": "/static/js/main.663e56c4.chunk.js"
+  },
+  {
+    "revision": "6bedc7d4e72b37a14961",
+    "url": "/static/js/runtime-main.059467c3.js"
+  },
+  {
+    "revision": "c878facff7601f97699567d97f0226d6",
+    "url": "/static/media/crown.c878facf.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
